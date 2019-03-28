77d8ebdd811d9eeadc288ea6bef9801a4fc9d6b8
lganzzzo <lganzzzo@gmail.com>
2019-03-28 19:58:48 +0200

    prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+apt-get install -y cmake
+apt-get install -y build-essential
+
+mkdir -p ./client/main/build
+mkdir -p ./server/main/build
+
+mkdir ./tmp
+
+cd tmp
+
+########################################
+# oatpp
+
+git clone https://github.com/oatpp/oatpp
+
+cd oatpp
+mkdir build && cd $_
+cmake -DOATPP_DISABLE_POOL_ALLOCATIONS=ON -DOATPP_DISABLE_ENV_OBJECT_COUNTERS=ON -DOATPP_THREAD_DISTRIBUTED_MEM_POOL_SHARDS_COUNT=1 ..
+
+make
+make install
+
+cd ../../
+
+########################################
+# oatpp
+
+git clone https://github.com/oatpp/oatpp-websocket
+
+cd oatpp-websocket
+mkdir build && cd $_
+cmake ..
+
+make
+make install
+
+cd ../../
+
+########################################
+
+cd ../

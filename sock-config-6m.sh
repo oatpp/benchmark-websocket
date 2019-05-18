67c08255349a58534f20558f37231dd5315baeff
lganzzzo <lganzzzo@gmail.com>
2019-05-18 22:52:33 +0300

    add sock-configs for 4m and 6m tests

--- sock-config-6m.sh
+++ sock-config-6m.sh
@@ -9,7 +9,7 @@ sysctl -w net.ipv4.tcp_max_syn_backlog=65535
 sysctl -w net.ipv4.ip_local_port_range="1025 65535"
 
 sysctl -w net.ipv4.tcp_mem="100000000 100000000 100000000"
-sysctl -w net.ipv4.tcp_rmem='4096 4096 4096'
+sysctl -w net.ipv4.tcp_rmem='2048 2048 2048'
 sysctl -w net.ipv4.tcp_wmem='4096 4096 4096'
 
 ## sysctl -w net.core.rmem_default=4096

0c4939e147b51a61807e4bd4dc31420388e5e18c
lganzzzo <lganzzzo@gmail.com>
2019-05-19 22:11:47 +0300

    sock-config for 5m test

--- sock-config-5m.sh
+++ sock-config-5m.sh
@@ -9,11 +9,12 @@ sysctl -w net.ipv4.tcp_max_syn_backlog=65535
 sysctl -w net.ipv4.ip_local_port_range="1025 65535"
 
 sysctl -w net.ipv4.tcp_mem="100000000 100000000 100000000"
+
 sysctl -w net.ipv4.tcp_rmem='2048 2048 2048'
 sysctl -w net.ipv4.tcp_wmem='4096 4096 4096'
 
-## sysctl -w net.core.rmem_default=4096
-## sysctl -w net.core.wmem_default=4608
+sysctl -w net.core.rmem_default=4096
+sysctl -w net.core.wmem_default=4608
 
-## sysctl -w net.core.rmem_max=4096
-## sysctl -w net.core.wmem_max=4608
+sysctl -w net.core.rmem_max=4096
+sysctl -w net.core.wmem_max=4608

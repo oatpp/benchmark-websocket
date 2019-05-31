0f8bbd0520fa2c0a1cfbe20b5c7c2b6236c3757e
lganzzzo <lganzzzo@gmail.com>
2019-05-31 20:45:35 +0300

    updated to the latest oatpp API

--- server/main/test/tests.cpp
+++ server/main/test/tests.cpp
@@ -17,12 +17,8 @@ void runTests() {
 int main() {
 
   oatpp::base::Environment::init();
-  oatpp::base::Environment::setLogger(new Logger());
 
   runTests();
-
-  oatpp::base::Environment::setLogger(nullptr);
-  oatpp::base::Environment::destroy();
 
   /* Print how much objects were created during app running, and what have left-probably leaked */
   /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
@@ -31,6 +27,8 @@ int main() {
   std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
 
   OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
+
+  oatpp::base::Environment::destroy();
 
   return 0;
 }

7eb2562ad9ea504c868c7c58677dc72da64eada7
lganzzzo <lganzzzo@gmail.com>
2019-05-31 20:52:19 +0300

    Fix client

--- client/main/src/App.cpp
+++ client/main/src/App.cpp
@@ -110,13 +110,10 @@ void run(const oatpp::base::CommandLineArguments& args) {
  *  main
  */
 int main(int argc, const char * argv[]) {
-  
-  oatpp::base::Environment::setLogger(new Logger());
+
   oatpp::base::Environment::init();
 
   run(oatpp::base::CommandLineArguments(argc, argv));
-  
-  oatpp::base::Environment::setLogger(nullptr); ///< free Logger
   
   /* Print how much objects were created during app running, and what have left-probably leaked */
   /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
